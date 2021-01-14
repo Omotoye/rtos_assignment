dc8a990052376643921015c6acc0300bb0963547
Omotoye Adekoya <adekoyaomotoye@gmail.com>
2021-01-14 19:00:01 +0100

    Added a README.md file and put proper commenting in the C codes

--- Driver/character_device_driver.c
+++ Driver/character_device_driver.c
@@ -1,9 +1,11 @@
-#include <linux/kernel.h> // help in building our driver module
+#include <linux/kernel.h> // help in building the driver module
 #include <linux/init.h>   // for kernel module programming to initialize the driver
 #include <linux/module.h>
 #include <linux/kdev_t.h> // for the device file
 #include <linux/fs.h>     // file structure
 #include <linux/cdev.h>
+#include <linux/errno.h>  // error codes
+#include <linux/types.h>  // size_t
 #include <linux/device.h> // for kmalloc
 #include <linux/slab.h>
 #include <linux/uaccess.h> // for communicating with the User Space
@@ -19,85 +21,76 @@ MODULE_AUTHOR("Omotoye Adekoya Shamsudeen");
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("A simple character device driver with write only function");
 
+// Defining prototypes for the init and exit functions
 static int __init chr_driver_init(void);
 static void __exit chr_driver_exit(void);
 
 // Defining the prototypes for the file operations
-static int my_open(struct inode *inode, struct file *file);
-static int my_release(struct inode *inode, struct file *file);
-//static ssize_t my_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
-static ssize_t my_write(struct file *filp, const char *buf, size_t len, loff_t *off);
+static int open_syscall(struct inode *inode, struct file *file);
+static int close_syscall(struct inode *inode, struct file *file);
+static ssize_t write_syscall(struct file *filp, const char *buf, size_t len, loff_t *off);
 
-// to define the file operation that will be done on the device
+// To defind file operations that will be done on the device
 static struct file_operations fops = {
     .owner = THIS_MODULE,
-    //.read = my_read,
-    .write = my_write,
-    .open = my_open,
-    .release = my_release,
+    .write = write_syscall,
+    .open = open_syscall,
+    .release = close_syscall,
 };
 
-static int my_open(struct inode *inode, struct file *file)
+// For the open() syscall
+static int open_syscall(struct inode *inode, struct file *file)
 {
-    /* Creating Physical Memory */
+    // Creating Physical Memory in the kernel
     if ((kernel_buffer = kmalloc(mem_size, GFP_KERNEL)) == 0)
     {
         printk(KERN_INFO "Cannot allocate the memory to the kernel...\n");
         return -1;
     }
-    //printk(KERN_INFO "Device file opened...\n");
     return 0;
 }
 
-static int my_release(struct inode *inode, struct file *file)
+// For the close() syscall
+static int close_syscall(struct inode *inode, struct file *file)
 {
-    //printk(KERN_INFO "Device FILE closed...\n");
     kfree(kernel_buffer);
     return 0;
 }
 
-//static ssize_t my_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
-//{
-//    copy_to_user(buf, kernel_buffer, mem_size);
-//    printk(KERN_INFO "Data read: DONE...\n");
-//    return mem_size;
-//}
-
-static ssize_t my_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
+// For the write() syscall
+static ssize_t write_syscall(struct file *filp, const char __user *buf, size_t len, loff_t *off)
 {
     copy_from_user(kernel_buffer, buf, len);
-    //printk(KERN_INFO "Data is written successfully...\n");
-    printk(KERN_INFO KERN_CONT "%s", kernel_buffer);
+    printk(KERN_CONT "%s", kernel_buffer);
     return len;
 }
 
+// Initialization function for the module_init function
 static int __init chr_driver_init(void)
 {
-    /* Allocating the Major Number */
+    // Allocating the Major Number
     if ((alloc_chrdev_region(&dev, 0, 1, "my_Dev")) < 0)
     {
         printk(KERN_INFO "Cannot allocate the major number..\n");
         return -1;
     }
-
     printk(KERN_INFO "Major = %d Minor = %d..\n", MAJOR(dev), MINOR(dev));
 
-    /* Creating cdev structure */
-
+    // Creating cdev structure
     cdev_init(&my_cdev, &fops);
 
-    /* Adding character device to the system */
+    // Adding character device to the system
     if ((cdev_add(&my_cdev, dev, 1)) < 0)
     {
-        printk(KERN_INFO "Cannot add the device to the system...\n");
+        printk(KERN_NOTICE "Cannot add the device to the system...\n");
         goto r_class; // if we are unable to add the class
     }
 
-    /* Creating struct class */
+    // Creating struct class
     if ((dev_class = class_create(THIS_MODULE, "my_class")) == NULL)
     {
         printk(KERN_INFO "Cannot create the struct class...\n");
-        goto r_class;
+        goto r_class; // r_class will unregister the character device
     }
 
     /* Creating device */
@@ -119,6 +112,7 @@ r_device:
     return -1;
 }
 
+// Exit function for module_exit function
 void __exit chr_driver_exit(void)
 {
     device_destroy(dev_class, dev);
@@ -128,7 +122,6 @@ void __exit chr_driver_exit(void)
     printk(KERN_INFO "Device Driver is removed successfully...\n");
 }
 
+// Setting the initialization function to the module_init and the exit function to the module_exit
 module_init(chr_driver_init);
 module_exit(chr_driver_exit);
-
-//We need to write some macros for the module__init and module__exit
--- multithreaded_app.c
+++ multithreaded_app.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <semaphore.h>
 #include <string.h>
 #include <fcntl.h>
 
@@ -10,38 +9,48 @@
 pthread_mutex_t task_mutex;
 pthread_mutexattr_t task_mutex_attr;
 
+// Type definition for the structure of the parameters for each of the threads
 typedef struct
 {
     int period;
     char write_message_1[3], write_message_2[3];
 } thread_param;
 
+// Task performed by each of the threads
 void *thread_task(void *args)
 {
     int fd;
     pthread_mutex_lock(&task_mutex);
     thread_param param = *(thread_param *)args;
-    fd = open("/dev/my_device", O_WRONLY);
-    write(fd, param.write_message_1, strlen(param.write_message_1) + 1);
+    if ((fd = open("/dev/my_device", O_WRONLY)) < 0)
+        perror("Open Failed");
+    if ((write(fd, param.write_message_1, strlen(param.write_message_1) + 1)) < 0)
+        perror("Write has failed");
     close(fd);
     pthread_mutex_unlock(&task_mutex);
     usleep(param.period);
     pthread_mutex_lock(&task_mutex);
-    fd = open("/dev/my_device", O_WRONLY);
-    write(fd, param.write_message_2, strlen(param.write_message_2) + 1);
+    if((fd = open("/dev/my_device", O_WRONLY)) < 0)
+        perror("Open Failed");
+    if ((write(fd, param.write_message_2, strlen(param.write_message_2) + 1)) < 0)
+        perror("Write Failed");
     close(fd);
     pthread_mutex_unlock(&task_mutex);
+
     pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
 {
     pthread_t thread[THREAD_NUM];
+
+    // Creating Mutex with priority ceiling attribute
     pthread_mutex_init(&task_mutex, &task_mutex_attr);
     pthread_mutexattr_init(&task_mutex_attr);
     pthread_mutexattr_setprotocol(&task_mutex_attr, PTHREAD_PRIO_PROTECT);
     pthread_mutexattr_setprioceiling(&task_mutex_attr, 10);
 
+    // Creating the struct for each thread argument
     thread_param thread_1, thread_2, thread_3;
     thread_1.period = 300;
     thread_2.period = 500;
@@ -58,6 +67,7 @@ int main(int argc, char *argv[])
     thread_param_array[1] = thread_2;
     thread_param_array[2] = thread_3;
 
+    // Creating the threads and passing the thread parameter as an argument
     for (int i = 0; i < THREAD_NUM; i++)
     {
         if (pthread_create(&thread[i], NULL, &thread_task, &thread_param_array[i]) != 0)
@@ -65,6 +75,8 @@ int main(int argc, char *argv[])
             perror("Failed to create thread");
         }
     }
+
+    // Joining all the threads
     for (int i = 0; i < THREAD_NUM; i++)
     {
         if (pthread_join(thread[i], NULL) != 0)
@@ -72,7 +84,9 @@ int main(int argc, char *argv[])
             perror("Failed to join thread");
         }
     }
-    printf("Finished Successfully\n");
+    printf("Finished Successfully\nCheck the Kernel Log the see the output\n");
+
+    // Destroying the mutex object 
     pthread_mutexattr_destroy(&task_mutex_attr);
     pthread_mutex_destroy(&task_mutex);
     return 0;
